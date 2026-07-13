ae6f39eaa9d062c96935e9aec74c5eac283b2de2
alesandrolima-hue <alesandrolima@pd7tech.com>
2026-07-13 15:20:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,58 +1,47 @@
 async function enviar(){
 
+try{
+
 let dados={
 
 nome:document.getElementById("nome").value,
-
 matricula:document.getElementById("matricula").value,
-
 empresa:document.getElementById("empresa").value,
-
 sala:document.getElementById("sala").value,
-
 tipo:document.getElementById("tipo").value,
-
 motivo:document.getElementById("motivo").value,
-
 assinatura:"",
-
 latitude:"",
-
 longitude:"",
-
 dispositivo:navigator.userAgent
 
 };
 
-const resposta=await fetch("https://script.google.com/macros/s/AKfycbzgQiAwIv_Yd4i5HO1vD81XfFeRcmuxLQoVhlLoK9Ygdz0mwMay6C8YTi3pDX72so9w7w/exec",{
+const resposta = await fetch("https://script.google.com/macros/s/AKfycbzgQiAwIv_Yd4i5HO1vD81XfFeRcmuxLQoVhlLoK9Ygdz0mwMay6C8YTi3pDX72so9w7w/exec",{
 
 method:"POST",
 
 headers:{
-
 "Content-Type":"text/plain;charset=utf-8"
-
 },
 
 body:JSON.stringify(dados)
 
 });
 
-const retorno=await resposta.json();
+console.log("Status:", resposta.status);
 
-if(retorno.sucesso){
+const texto = await resposta.text();
 
-alert("Registro salvo com sucesso!");
+console.log("Resposta:", texto);
 
-document.querySelectorAll("input,textarea").forEach(c=>c.value="");
+alert(texto);
 
-document.getElementById("sala").selectedIndex=0;
+}catch(erro){
 
-document.getElementById("tipo").selectedIndex=0;
+console.error(erro);
 
-}else{
-
-alert(retorno.erro);
+alert(erro);
 
 }
 
