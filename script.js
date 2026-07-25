12740be5d7bad8557c418aca5c29f34215969972
alesandrolima-hue <alesandrolima@pd7tech.com>
2026-07-25 15:46:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // ==========================
 
 // IMPORTANTE: Cole sua URL real do Apps Script aqui
-const URL_API = "https://script.google.com/macros/s/AKfycbw9hy9cD3c2-zLO9CkxZqzWR2NxXVeA3oNg18amZkXC6neC4MwhQiHZIDtogW6gA7eVkg/exec";
+const URL_API = "https://script.google.com/macros/s/AKfycbyKAQbYe4F8bkwKaUxhzOEsPfLnnhGl0DJXeknah3hKXXK79brf4F437NkmrlvrasZ_Uw/exec";
 
 let canvasResp, canvasRespCtx;
 let canvasSol, canvasSolCtx;
